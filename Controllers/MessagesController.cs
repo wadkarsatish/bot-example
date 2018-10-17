3081d6b111cc49757e74f16538949c526071d888
Wadkar Satish <satish.wadkar@tieto.com>
2018-10-17 17:40:22 +0530

    first commit

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,89 @@
+﻿namespace EmergencyServicesBot
+{
+    using EmergencyServicesBot.Model;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.FormFlow;
+    using Microsoft.Bot.Connector;
+    using System.Diagnostics;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        [System.Web.Http.Description.ResponseType(typeof(void))]
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity != null)
+            {
+                switch (activity.GetActivityType())
+                {
+                    case ActivityTypes.Message:
+                        await Conversation.SendAsync(activity, MakeRootDialog);
+                        break;
+
+                    case ActivityTypes.ConversationUpdate:
+                    case ActivityTypes.ContactRelationUpdate:
+                    case ActivityTypes.Typing:
+                    case ActivityTypes.DeleteUserData:
+                    default:
+                        Trace.TraceError($"Unknown activity type ignored: {activity.GetActivityType()}");
+                        break;
+                }
+            }
+
+            //if (activity.Type == ActivityTypes.Message)
+            //{
+            //    await Conversation.SendAsync(activity, () => Chain.From(() => FormDialog.FromForm(Appointment.BuildForm)));
+            //    //await Conversation.SendAsync(activity, () => new Dialogs.AdaptiveCardDialog());
+            //}
+            else
+            {
+                this.HandleSystemMessage(activity);
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        internal static IDialog<Appointment> MakeRootDialog()
+        {
+            return Chain.From(() => FormDialog.FromForm(Appointment.BuildForm));
+        }
+
+        private Activity HandleSystemMessage(Activity activity)
+        {
+            if (activity.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (activity.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (activity.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing that the user is typing
+            }
+            else if (activity.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Model/OrderDetails.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+
+namespace EmergencyServicesBot.Model
+{
+    public enum Specialty
+    {
+        Dentist, GeneralPhysician, Psychiatist,
+        Cardiologist, PhysioTherapist
+    }
+
+    // Appointment is the simple form you will fill out to set up an appointment with Doctor.    
+    [Serializable]
+    public class Appointment
+    {
+        [Prompt("Please enter {&}?")]
+        public string PatientId { get; set; }
+
+        [Prompt("When would you like to book your {&}?")]
+        public DateTime AppointmentDate { get; set; }
+
+        [Prompt("What is the {&}")]
+        public string PatientName { get; set; }
+
+        [Prompt("What are the {&} you are looking for? {||}")]
+        public Specialty? Specialties;
+
+        [Prompt("Any {&} to the Doctor?")]
+        public string SpecialInstructions { get; set; }
+
+        public static IForm<Appointment> BuildForm()
+        {
+            OnCompletionAsyncDelegate<Appointment> processAppointment =
+            async (context, state) =>
+            {
+                IMessageActivity reply = context.MakeMessage();
+                reply.Speak = reply.Text = $"We are confirming your appointment for { state.PatientName} at { state.AppointmentDate.ToString(CultureInfo.InvariantCulture)}, please be on time. " + " Reference ID: " + Guid.NewGuid().ToString().Substring(0, 5);
+
+                // Save State to database here...
+                await context.PostAsync(reply);
+
+            };
+            return new FormBuilder<Appointment>()
+            .Message("Welcome, I'm Dr.Bot ! I can help with fix an appointment with Doctor.")
+            .OnCompletion(processAppointment)
+            .Build();
+        }
+    };
+}
